6ba42f457a60766f813bce8afc16e50d33fe7809
Simon Egli <simon.e98zh@gmail.com>
2025-08-30 14:26:56 +0200

    Initial project version

---
+++ src/main/java/ch/simi1892/mundartdestages/MundartdestagesApplication.java
@@ -0,0 +1,13 @@
+package ch.simi1892.mundartdestages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundartdestagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundartdestagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/simi1892/mundartdestages/MundartdestagesApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.simi1892.mundartdestages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundartdestagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
